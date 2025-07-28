a2b543a8a40dbc706440cb990ddfb00ea8c42612
spidey.ai45 <jeevan171767@gmail.com>
2025-07-28 06:33:52 +0530

    Update leap year checker.py

--- basic practice/leap year/leap year checker.py
+++ basic practice/leap year/leap year checker.py
@@ -2,9 +2,10 @@ year = int (input("enter the year:"))
 
 leap = False
 
-if year%400 == 0 and year%100 != 0 :
+if year%400 == 0 :
+    leap = True
+elif  year%100 == 0 :
     leap = False
-
 elif year%4 ==0 :
     leap = True
 
